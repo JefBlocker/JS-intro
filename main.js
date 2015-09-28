9bb8367b7593aa94237a5c097463b768cc73f22f
Jef Blocker <jefblocker@gmail.com>
2015-09-28 13:36:03 -0400

    Completed the answers in the main.js

---
+++ main.js
@@ -0,0 +1,81 @@
+Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: undefined
+
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: "Tim"
+
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: "JD"
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: undefined
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: undefined
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+// The answer shoud not be written in a comment.
+var born = 1983;
+var today = 2015;
+
+// Answers Below:
+
+var today = 2015;
+undefined
+var born = 1983;
+undefined
+var age = today - born;
+undefined
+"Tim is " + age + " years old";
+"Tim is 32 years old"
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+var yourName = "Jef"
+undefined
+var instructorName = "JD";
+undefined
+yourName + "'s instructor is " + instructorName;
+"Jef's instructor is JD"
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+undefined
+statement;
+"Jef is taking a class at The Iron Yard, my instructor's name is JD"
